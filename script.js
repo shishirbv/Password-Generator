bdb09708cd66201d9f217369ea092b2f7b555a5d
shishirbv <158254323+shishirbv@users.noreply.github.com>
2026-04-27 16:42:39 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function generatePassword() {
+    let uppercase = document.getElementById("uppercase").checked;
+    let lowercase = document.getElementById("lowercase").checked;
+    let numbers = document.getElementById("numbers").checked;
+    let symbols = document.getElementById("symbols").checked;
+    let length = document.getElementById("length").value;
+
+    let chars = "";
+
+    if (uppercase) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (lowercase) chars += "abcdefghijklmnopqrstuvwxyz";
+    if (numbers) chars += "0123456789";
+    if (symbols) chars += "!@#$%^&*()_+";
+
+    if (chars === "") {
+        alert("Select at least one option!");
+        return;
+    }
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        let randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+
+    document.getElementById("password").value = password;
+}
